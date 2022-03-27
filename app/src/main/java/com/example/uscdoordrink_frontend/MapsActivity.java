e5ffb1413c4828c2b8fb8dca48e2beb6cf29c45d
Yuxuan Liao <liaoyuxu@gmail.com>
2022-03-27 01:02:14 -0700

    Added fab to MapsActivity

--- app/src/main/java/com/example/uscdoordrink_frontend/MapsActivity.java
+++ app/src/main/java/com/example/uscdoordrink_frontend/MapsActivity.java
@@ -3,6 +3,8 @@ package com.example.uscdoordrink_frontend;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,24 +12,40 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.uscdoordrink_frontend.databinding.ActivityMapsBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+//import com.example.uscdoordrink_frontend.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+//    private ActivityMapsBinding binding;
+    private static String TAG = "MapsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+//        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_maps);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+//        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .add(R.id.map_container_view, mapFragment)
+//                .commit();
+        FloatingActionButton fab = findViewById(R.id.fab_to_personal_profile);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Fab is pressed successfully");
+            }
+        });
     }
 
     /**
